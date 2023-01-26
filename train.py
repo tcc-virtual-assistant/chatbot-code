b0b9a4d8406f60a23ac2b8ed3ded8cd330780be5
nychavs <chavesnaiury@gmail.com>
2023-01-26 11:25:12 -0300

    imported torch

--- train.py
+++ train.py
@@ -3,6 +3,10 @@ from nltk_utils import tokenize, stem, bag_of_words
 from unidecode import unidecode
 import numpy as np
 
+import torch 
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
 with open('intents.json', 'r') as file:
     intents = json.load(file)
 
