ce51d666dabbafa335aac83e66ecf3aa21074989
nychavs <chavesnaiury@gmail.com>
2023-01-31 10:21:07 -0300

    add training data

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from unidecode import unidecode
 import numpy as np
 
 import torch 
-import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from . import nn as nn
 
 with open('intents.json', 'r') as file:
     intents = json.load(file)
@@ -38,3 +38,24 @@ for (pattern_tokenized, tag) in xy:
     y_train.append(tag_numbers)
 x_train = np.array(x_train)
 y_train = np.array(y_train)
+
+#its necessary this class with this functions to create a custom dataset using pytorch
+#so this is useful bc we can iterate automatically and better training
+#initialize with all informations
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(x_train) #number of examples
+        self.x_data = x_train  
+        self.y_data = y_train
+
+#returns a sample from the dataset at the given index
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+#returns the number of samples in our dataset
+    def __len__(self):
+        return self.n_samples
+         
+batch_size = 8
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
