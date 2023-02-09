415286160bfb1642ed7da81e0f70e8619a692969
nychavs <chavesnaiury@gmail.com>
2023-02-09 11:15:14 -0300

    add fastapi and uvicorn

--- chat.py
+++ chat.py
@@ -1,52 +1,64 @@
 import random
 import json
-import train
+# import train
 import torch
-
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from fastapi import FastAPI
+import uvicorn
 
-with open('intents.json', 'r', encoding="utf8") as json_data:
-    intents = json.load(json_data)
+chatApi = FastAPI()
 
-FILE = "data.pth"
-data = torch.load(FILE)
+@chatApi.get('/chat')
+async def home():
+    return "aaaaaa"
 
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
+def chatbot():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
+    with open('intents.json', 'r', encoding="utf8") as json_data:
+        intents = json.load(json_data)
 
-bot_name = "Ávila"
-print("Vamos testar! (Escreva 'sair' para sair)")
-while True:
-    sentence = input("Você: ")
-    if sentence == "sair":
-        print( "Espero que eu tenha conseguido te ajudar! Tenha um ótimo dia!")
-        break
+    FILE = "data.pth"
+    data = torch.load(FILE)
 
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
 
-    tag = tags[predicted.item()]
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: Desculpe, eu não consegui compreender...")
+    bot_name = "Ávila"
+    print("Vamos testar! (Escreva 'sair' para sair)")
+    while True:
+        sentence = input("Você: ")
+        if sentence == "sair":
+            print( "Espero que eu tenha conseguido te ajudar! Tenha um ótimo dia!")
+            break
+
+        sentence = tokenize(sentence)
+        X = bag_of_words(sentence, all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(device)
+
+        output = model(X)
+        _, predicted = torch.max(output, dim=1)
+
+        tag = tags[predicted.item()]
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.75:
+            for intent in intents['intents']:
+                if tag == intent["tag"]:
+                    print(f"{bot_name}: {random.choice(intent['responses'])}")
+        else:
+            print(f"{bot_name}: Desculpe, eu não consegui compreender...")
+
+if __name__ == '__main__':
+    uvicorn.run(app=chatApi, host="127.0.0.1", port=777)
